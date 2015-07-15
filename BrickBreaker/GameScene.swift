2265ddc9203be0596a1ce2bb40bf0fc3fa8eabdd
Atikur Rahman <atik.bd08@gmail.com>
2015-07-15 10:58:35 +0600

    add method to create ball

--- BrickBreaker/GameScene.swift
+++ BrickBreaker/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene {
     var paddle: SKSpriteNode!
     var touchLocation: CGPoint!
     
+    // MARK: -
+    
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         
         for touch in (touches as! Set<UITouch>) {
@@ -42,12 +44,35 @@ class GameScene: SKScene {
         }
     }
     
+    // MARK: -
+    
+    func createBallWithLocation(position: CGPoint, velocity: CGVector) -> SKSpriteNode {
+        let ball = SKSpriteNode(imageNamed: "BallBlue")
+        ball.name = name
+        ball.position = position
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width * 0.5)
+        ball.physicsBody?.friction = 0.0
+        ball.physicsBody?.linearDamping = 0.0
+        ball.physicsBody?.restitution = 1.0
+        ball.physicsBody?.velocity = velocity
+        
+        addChild(ball)
+        return ball
+    }
+    
+    
     // MARK: - Initializers
     
     override init(size: CGSize) {
         super.init(size: size)
         
         backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
+        
+        self.physicsWorld.gravity = CGVectorMake(0, 0)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        
+        createBallWithLocation(CGPointMake(self.size.width/2, self.size.height/2), velocity: CGVectorMake(40, 180))
         
         paddle = SKSpriteNode(imageNamed: "Paddle")
         paddle.position = CGPointMake(self.size.width/2, 90)
